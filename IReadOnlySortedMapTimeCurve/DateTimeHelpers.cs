7c7e7957b332d65e55bfc0bf4a7eb2814281959a
KonorovDA <KonorovDA@KONOROV>
2025-09-02 14:43:42 +0400

    Add project files.

---
+++ IReadOnlySortedMapTimeCurve/DateTimeHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IReadOnlySortedMapTimeCurve
+{
+    public static class DateTimeHelpers
+    {
+        public static DateTime CreateFromByteArray(byte[] bytes)
+        {
+            if (bytes is null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            if (bytes.Length != 8)
+                throw new ArgumentException("Byte array is wrong");
+
+            var ticks = BitConverter.ToInt64(bytes, 0);
+            
+            return new DateTime(ticks);
+        }
+
+        public static byte[] CreateFromDateTime(DateTime dateTime)
+        {
+            var ticks = dateTime.Ticks;
+
+            return BitConverter.GetBytes(ticks);
+        }
+    }
+}
---
+++ ReadOnlySortedMapTimeCurveTests/HelpersTests.cs
@@ -0,0 +1,60 @@
+﻿using IReadOnlySortedMapTimeCurve;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlySortedMapTimeCurveTests
+{
+    [TestFixture]
+    public class HelpersTests
+    {
+        [Test]
+        public void ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => DateTimeHelpers.CreateFromByteArray(null));
+        }
+
+        //[Test]
+        //public void ShouldBeRightArraySize()
+        //{
+        //    // arrange
+        //    var dt = DateTime.Now;
+
+        //    // act
+        //    var bytes = DateTimeHelpers.CreateFromDateTime(dt);
+
+        //    // assert 
+        //    Assert.That(8, Is.EqualTo(bytes.Length));
+        //}
+
+        [Test]
+        public void ShouldBeRightArraySize()
+        {
+            // arrange
+            byte[] byteArray = new byte[4];
+
+            // act
+
+            // assert 
+            Assert.Throws<ArgumentException> (() => DateTimeHelpers.CreateFromByteArray(byteArray), "Byte array is wrong");
+        }
+
+        [Test]
+        public void ShouldConvertToDateTime()
+        {
+            // arrange
+            var dt = DateTime.Now;
+            var bytes = DateTimeHelpers.CreateFromDateTime(dt);
+
+            // act
+            var dateTime = DateTimeHelpers.CreateFromByteArray(bytes);
+
+            // assert
+            Assert.That(dt, Is.EqualTo(dateTime));
+        }        
+    }
+}
