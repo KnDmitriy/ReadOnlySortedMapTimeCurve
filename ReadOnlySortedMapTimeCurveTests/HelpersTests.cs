6e53252dcb39ab605e07617ab9c53c7f5eaf6da3
KonorovDA <KonorovDA@KONOROV>
2025-09-02 16:17:40 +0400

    I'm in progress

--- ReadOnlySortedMapTimeCurveTests/HelpersTests.cs
+++ ReadOnlySortedMapTimeCurveTests/HelpersTests.cs
@@ -1,5 +1,6 @@
 ﻿using IReadOnlySortedMapTimeCurve;
 using NUnit.Framework;
+using Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +13,23 @@ namespace ReadOnlySortedMapTimeCurveTests
     [TestFixture]
     public class HelpersTests
     {
+
+        [SetUp]
+        public void Setup()
+        {
+            curve = new PieList<double, double>();
+            curve.Insert(1000.1, 1.6);
+            curve.Insert(1001.1, 1.3);
+            curve.Insert(1002.1, -0.5);
+            curve.Insert(1003.1, 3.2);
+            curve.Insert(1004.1, 0.2);
+        }
+
         [Test]
         public void ShouldThrowArgumentNullException()
         {
             Assert.Throws<ArgumentNullException>(() => DateTimeHelpers.CreateFromByteArray(null));
         }
-
-        //[Test]
-        //public void ShouldBeRightArraySize()
-        //{
-        //    // arrange
-        //    var dt = DateTime.Now;
-
-        //    // act
-        //    var bytes = DateTimeHelpers.CreateFromDateTime(dt);
-
-        //    // assert 
-        //    Assert.That(8, Is.EqualTo(bytes.Length));
-        //}
 
         [Test]
         public void ShouldBeRightArraySize()
@@ -55,6 +55,25 @@ namespace ReadOnlySortedMapTimeCurveTests
 
             // assert
             Assert.That(dt, Is.EqualTo(dateTime));
-        }        
+        }
+
+        private PieList<double, double> curve;
+
+
+        //[Test]
+        //public void ShouldGetDataFromCurve()
+        //{
+        //    // arrange
+        //    Setup();
+
+        //    // act
+        //    double val = curve.GetValue(10);
+
+        //    // assert
+        //    Assert.That(1000, Is.EqualTo((double)val));
+
+        //}
+
+
     }
 }
