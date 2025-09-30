882cdb1ffc36e79fc1cbe10387e2ae0c53ea89a4
KonorovDA <KonorovDA@KONOROV>
2025-09-30 11:45:46 +0400

    Изменен интерфейс IDepthToTimeIndexConverter.
    Из метода Convert вынесена часть кода в отдельный метод GetTicksByBinarySearchIndex.
    
    Добавлена ссылка на класс Format в основную сборку.

--- IReadOnlySortedMapTimeCurve/IDepthToTimeIndexConverter.cs
+++ IReadOnlySortedMapTimeCurve/IDepthToTimeIndexConverter.cs
@@ -1,9 +1,11 @@
 ﻿using Collections;
+using Format;
 
 namespace TimeReadOnlySortedMap
 {
     public interface IDepthToTimeIndexConverter
     {
         IReadOnlySortedMap<double, T> Convert<T>(IReadOnlySortedMap<double, T> valuesByDepthMap);
+        IReadOnlySortedMap<double, RecordWaveValue> Convert(IReadOnlySortedMap<double, RecordWaveValue> valuesByDepthMap);
     }
 }
